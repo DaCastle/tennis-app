7ae2dfb5763775d7c71a58fb2c1c4dbacc001528
DaCastle <dakota.castleberg18@gmail.com>
2019-08-02 15:16:33 -0500

    added Dashboard and Notifications and PostList to layout

---
+++ src/components/posts/PostList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const PostList = () => {
+
+    return (
+        <div className="project list section">
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Post Title</span>
+                    <p>Posted by Dakota Castleberg</p>
+                    <p className="grey-text">the date</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Post Title</span>
+                    <p>Posted by Dakota Castleberg</p>
+                    <p className="grey-text">the date</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Post Title</span>
+                    <p>Posted by Dakota Castleberg</p>
+                    <p className="grey-text">the date</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default PostList;
