a94cefac4e9ad34e3be0e3149b225eaf8b2fa364
DaCastle <dakota.castleberg18@gmail.com>
2019-08-04 13:33:22 -0500

    adding dummy data for post summary/details

--- src/components/posts/PostSummary.js
+++ src/components/posts/PostSummary.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const PostSummary = () => {
+const PostSummary = ({ post }) => {
     return (
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Post Title</span>
+                <span className="card-title">{post.title}</span>
                 <p>Posted by Dakota Castleberg</p>
                 <p className="grey-text">the date</p>
             </div>
