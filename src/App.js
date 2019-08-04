15ac8a940b9bd6fcd591eb309acc55f1190c2eb7
DaCastle <dakota.castleberg18@gmail.com>
2019-08-04 10:42:56 -0500

    created SignIn and SignUp layouts, added to router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import NavBar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import PostDetails from './components/posts/PostDetails';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+
 
 class App extends Component {
   render() {
@@ -13,6 +16,8 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={Dashboard} />
             <Route path='/post/:id' component={PostDetails} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
           </Switch>
         </div>
       </BrowserRouter>
