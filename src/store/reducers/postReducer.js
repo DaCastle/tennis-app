228a0c731c4fe984d05974f7a716e6be696b5ccc
DaCastle <dakota.castleberg18@gmail.com>
2019-08-04 14:26:50 -0500

    adding raect Thunk as middleware to allow managing async data to be handled via dispatch action before getting passed to the reducer

--- src/store/reducers/postReducer.js
+++ src/store/reducers/postReducer.js
@@ -3,11 +3,14 @@ const initState = {
         { id: '1', title: 'Looking for hitting partners?', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, esse hic. Similique minus ipsa repellendus fugit, debitis consectetur, qui earum soluta voluptates obcaecati modi natus aperiam ipsam, ad nihil porro.' },
         { id: '2', title: 'Looking to hit at Zachary Playfield courts', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, esse hic. Similique minus ipsa repellendus fugit, debitis consectetur, qui earum soluta voluptates obcaecati modi natus aperiam ipsam, ad nihil porro.' },
         { id: '3', title: 'other good courts near by?', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, esse hic. Similique minus ipsa repellendus fugit, debitis consectetur, qui earum soluta voluptates obcaecati modi natus aperiam ipsam, ad nihil porro.' }
-
     ]
 };
 
 const postReducer = (state = initState, action) => {
+    switch (action.type) {
+        case 'CREATE_POST':
+            console.log('created post', action.post)
+    }
     return state;
 }
 
