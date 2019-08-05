e28be9a3a20c504d6417f8fc607180e96cae56be
DaCastle <dakota.castleberg18@gmail.com>
2019-08-05 17:36:01 -0500

    added cloud function for new user sign up notification

--- functions/index.js
+++ functions/index.js
@@ -10,8 +10,8 @@ admin.initializeApp(functions.config().firebase);
 const createNotification = (notification => {
     return admin.firestore().collection('notifications')
         .add(notification)
-        .then(document => console.log('notification added', document))
-})
+        .then(document => console.log('notification added', document));
+});
 
 exports.postCreated = functions.firestore
     .document('posts/{postId}')
@@ -24,5 +24,25 @@ exports.postCreated = functions.firestore
             time: admin.firestore.FieldValue.serverTimestamp()
         }
         return createNotification(notification);
+    });
+
+
+exports.userJoined = functions.auth.user().
+    onCreate(user => {
+
+        return admin.firestore().collection('users')
+            .doc(user.uid).get()
+            .then(doc => {
+
+                const newUser = doc.data();
+                const notification = {
+                    content: 'Joined the party',
+                    user: `${newUser.firstName} ${newUser.lastName}`,
+                    time: admin.firestore.FieldValue.serverTimestamp()
+                }
+
+                return createNotification(notification);
+
+            })
 
     })
