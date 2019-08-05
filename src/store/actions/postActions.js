1354b889417ca87a6278e062b5fda6fc1d74bf8e
DaCastle <dakota.castleberg18@gmail.com>
2019-08-04 20:04:28 -0500

    testing adding data to Firestore

--- src/store/actions/postActions.js
+++ src/store/actions/postActions.js
@@ -1,6 +1,17 @@
 export const createPost = (post) => {
     return (dispatch, getState, { getFirebase, getFirestore }) => {
         // make async call to database
-        dispatch({ type: 'CREATE_POST', post })
+        const firestore = getFirestore();
+        firestore.collection('posts').add({
+            ...post,
+            authorFirstName: 'Dak',
+            authorLastName: 'Cas',
+            authorId: 5,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({ type: 'CREATE_POST', post })
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_POST_ERROR', err })
+        })
     }
 }
