9e70e75469e6a71e0f5cb69f74b1f66f7b7be7f1
DaCastle <dakota.castleberg18@gmail.com>
2019-08-23 07:39:02 -0500

    SignUp - descriptive text letting usersknow email/phone number will be visible to users

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -29,7 +29,7 @@ class SignUp extends Component {
                     <h5 className="grey-text text-darken-3">Sign Up</h5>
                     <div className="input-field">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" required onChange={this.handleChange} />
+                        <input type="email" id="email" required placeholder='visible to other users' onChange={this.handleChange} />
                     </div>
                     <div className="input-field">
                         <label htmlFor="password">Password</label>
@@ -45,7 +45,7 @@ class SignUp extends Component {
                     </div>
                     <div className="input-field">
                         <label htmlFor="phoneNumber">Phone Number</label>
-                        <input type="text" id="phoneNumber" placeholder='optional but preferred' onChange={this.handleChange} />
+                        <input type="text" id="phoneNumber" placeholder='optional but preferred - visible to other users' onChange={this.handleChange} />
                     </div>
                     <div className="input-field">
                         <label htmlFor="ntrpLevel">NTRP Level</label>
